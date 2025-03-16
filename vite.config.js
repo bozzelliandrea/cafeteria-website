f92f449be7138f9405ec6232b66bb501bfdc6ae2
bozzelliandrea <bozzelliandrea@yahoo.it>
2025-03-16 16:05:24 +0100

    fix: vite prod base url

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: Number(env.FRONTEND_PORT),
     },
+    base: '/cafeteria-website/',
     root: 'src',
     envDir: '../',
     build: {
