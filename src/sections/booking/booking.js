47905692456a08a987cbd5d22f9054fbd8bfaf77
bozzelliandrea <bozzelliandrea@yahoo.it>
2025-03-16 18:03:33 +0100

    feat: contact sections

--- src/sections/booking/booking.js
+++ src/sections/booking/booking.js
@@ -1,8 +1,9 @@
 import './booking.scss';
 import template from './booking.html?raw';
+import coffeecin from '../../../assets/coffee_cin.jpg';
 
 document.addEventListener('WebAppInvoker_ContentLoading', () => {
-  console.log('FROM BOOKING');
+  document.getElementById('coffeecin').src = coffeecin;
 });
 
 export default template;
--- src/sections/menu/menu.js
+++ src/sections/menu/menu.js
@@ -9,7 +9,7 @@ function createMenuItem({ name, description, price }) {
   // Create the image div
   const imgDiv = document.createElement('div');
   imgDiv.classList.add('menu-item_icon');
-  imgDiv.style.backgroundImage = 'url(images/dish-1.jpg)'; // You can customize this URL
+  imgDiv.style.backgroundImage = 'url(images/dish-1.jpg)';
 
   const imgContainerDiv = document.createElement('div');
   imgContainerDiv.classList.add('menu-item_icon-container');
@@ -19,7 +19,7 @@ function createMenuItem({ name, description, price }) {
   const contentHeaderDiv = document.createElement('div');
   contentHeaderDiv.classList.add('menu-item_content');
 
-  // Create the inner content div (d-flex text align-items-center)
+  // Create the inner content div
   const innerContentDiv = document.createElement('div');
   innerContentDiv.classList.add('menu-item_content-header');
 
@@ -36,7 +36,7 @@ function createMenuItem({ name, description, price }) {
   innerContentDiv.appendChild(h3);
   innerContentDiv.appendChild(priceSpan);
 
-  // Create the description div (d-block)
+  // Create the description div
   const descriptionDiv = document.createElement('div');
   descriptionDiv.classList.add('menu-item_content-description');
   const descriptionPara = document.createElement('p');
@@ -63,7 +63,7 @@ const STARTER_LIST = [
   {
     name: 'Starter Dish Two',
     description: 'A description of this amazing dish.',
-    price: 12,
+    price: '12.00',
   },
   {
     name: 'Starter Dish Three',
@@ -73,26 +73,95 @@ const STARTER_LIST = [
   {
     name: 'Starter Dish Four',
     description: 'A description of this amazing dish.',
-    price: 7,
+    price: '7.00',
   },
 ];
 
-function buildMenuSectionContainer(containerName, configArray) {
-  const container = document.getElementById(containerName);
+const MAIN_LIST = [
+  {
+    name: 'Main Dish One',
+    description: 'A description of this amazing dish.',
+    price: '6.50',
+  },
+  {
+    name: 'Main Dish Two',
+    description: 'A description of this amazing dish.',
+    price: '12.00',
+  },
+  {
+    name: 'Main Dish Three',
+    description: 'A description of this amazing dish.',
+    price: '8.40',
+  },
+  {
+    name: 'Main Dish Four',
+    description: 'A description of this amazing dish.',
+    price: '7.00',
+  },
+];
+
+const DESSERT_LIST = [
+  {
+    name: 'Dessert One',
+    description: 'A description of this amazing dish.',
+    price: '5.50',
+  },
+  {
+    name: 'Dessert Two',
+    description: 'A description of this amazing dish.',
+    price: '6.00',
+  },
+  {
+    name: 'Dessert Three',
+    description: 'A description of this amazing dish.',
+    price: '4.50',
+  },
+  {
+    name: 'Dessert Four',
+    description: 'A description of this amazing dish.',
+    price: '3.50',
+  },
+];
+
+const DRINKS_LIST = [
+  {
+    name: 'Drink One',
+    description: 'A description of this amazing dish.',
+    price: '4.50',
+  },
+  {
+    name: 'Drink Two',
+    description: 'A description of this amazing dish.',
+    price: '5.50',
+  },
+  {
+    name: 'Drink Three',
+    description: 'A description of this amazing dish.',
+    price: '4.50',
+  },
+  {
+    name: 'Drink Four',
+    description: 'A description of this amazing dish.',
+    price: '3.00',
+  },
+];
+
+function buildMenuSectionContainer(containerId, configArray) {
+  const container = document.getElementById(containerId);
   if (container) {
     configArray.forEach((config) =>
       container.appendChild(createMenuItem(config)),
     );
   } else {
-    console.error(`${containerName} not found in DOM!`);
+    console.error(`${containerId} not found in DOM!`);
   }
 }
 
 document.addEventListener('WebAppInvoker_ContentLoading', () => {
   buildMenuSectionContainer('starter-container', STARTER_LIST);
-  buildMenuSectionContainer('main-menu-container', STARTER_LIST);
-  buildMenuSectionContainer('dessert-container', STARTER_LIST);
-  buildMenuSectionContainer('drinks-container', STARTER_LIST);
+  buildMenuSectionContainer('main-menu-container', MAIN_LIST);
+  buildMenuSectionContainer('dessert-container', DESSERT_LIST);
+  buildMenuSectionContainer('drinks-container', DRINKS_LIST);
 });
 
 export default template;
